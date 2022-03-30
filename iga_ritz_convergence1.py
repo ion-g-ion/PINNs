3fad95b261ca131b6072192eb99ce57331176cc4
ion <ion.ion.gabriel@gmail.com>
2022-03-30 10:08:15 +0300

    added interface corrections

--- iga_ritz_convergence1.py
+++ iga_ritz_convergence1.py
@@ -40,27 +40,26 @@ def interface_function2d(nd, endpositive, endzero, nn):
     return fret
 
 class Model(pinns.PINN):
-    def __init__(self, rand_key):
+    def __init__(self, rand_key, nl = 16, N = [64,64], num_blocks = 4):
         super().__init__()
         self.key = rand_key
 
-        N = [64,64]
-        nl = 8
+
         
         block = stax.serial(stax.FanOut(2),stax.parallel(stax.serial(stax.Dense(nl), stax.Tanh, stax.Dense(nl), stax.Tanh),stax.Dense(nl)),stax.FanInSum)
     
-        self.add_neural_network('u1',stax.serial(block,block,block,block,block,stax.Dense(1)),(-1,2))
-        self.add_neural_network('u2',stax.serial(block,block,block,block,block,stax.Dense(1)),(-1,2))
-        self.add_neural_network('u12',stax.serial(stax.Dense(nl),stax.Tanh,stax.Dense(nl),stax.Tanh,stax.Dense(1)),(-1,1))
+        self.add_neural_network('u1',stax.serial(*([block]*num_blocks),stax.Dense(1)),(-1,2))
+        self.add_neural_network('u2',stax.serial(*([block]*num_blocks),stax.Dense(1)),(-1,2))
+        self.add_neural_network('u12',stax.serial(stax.Dense(nl),stax.Tanh,stax.Dense(nl),stax.Tanh,stax.Dense(nl),stax.Tanh,stax.Dense(1)),(-1,1))
         # self.add_neural_network('u',stax.serial(stax.Dense(nl), stax.Tanh, stax.Dense(nl), stax.Tanh,stax.Dense(nl), stax.Tanh, stax.Dense(nl), stax.Tanh,stax.Dense(nl), stax.Tanh, stax.Dense(nl), stax.Tanh,stax.Dense(1)),(-1,2))
         self.init_points(N)
         
         self.interface12 = interface_function2d(1,0.0,1.0,self.neural_networks['u12'])
         self.interface21 = interface_function2d(1,1.0,0.0,self.neural_networks['u12'])
         
-        self.eps1 = 1/1000
+        self.eps1 = 1/100
         self.eps2 = 1/2
-        self.U = 100
+        self.U = 10
         
     def init_points(self, N):
         
@@ -124,86 +123,79 @@ class Model(pinns.PINN):
 rnd_key = jax.random.PRNGKey(123)
 
 
-model = Model(rnd_key)
+def simmulation( nl = 16, N = [64,64], num_blocks = 2):
+    print('Width ',nl,' blocks ',num_blocks)
+    results = {'width' : nl, 'blocks' : num_blocks, 'integration_grid_size': N}
+    model = Model(rnd_key, nl = nl, N = N, num_blocks = num_blocks)
 
-w0 = model.init_unravel()
-weights = w0
-
-
-loss_compiled = jax.jit(model.loss_handle)
-lossgrad_compiled = jax.jit(model.lossgrad_handle)
-
-print('Starting optimization')
+    w0 = model.init_unravel()
+    weights = w0
 
 
-def loss_grad(w):
-    l, gr = lossgrad_compiled(jnp.array(w))
-    return np.array( l.to_py() ), np.array( gr.to_py() ) 
+    loss_compiled = jax.jit(model.loss_handle)
+    lossgrad_compiled = jax.jit(model.lossgrad_handle)
 
-tme = datetime.datetime.now()
-#results = jax.scipy.optimize.minimize(loss_grad, x0 = weights_vector, method = 'bfgs', options = {'maxiter': 10})
-# result = scipy.optimize.minimize(loss_grad, x0 = w0.to_py(), method = 'BFGS', jac = True, tol = 1e-8, options = {'disp' : True, 'maxiter' : 400}, callback = lambda x: print(loss_compiled(x)))
-result = scipy.optimize.minimize(loss_grad, x0 = weights.to_py(), method = 'L-BFGS-B', jac = True, tol = 1e-9, options = {'disp' : True, 'maxiter' : 1000, 'iprint': 1})
-tme = datetime.datetime.now() - tme
-
-weights = model.weights_unravel(jnp.array(result.x))
-
-print()
-print('Elapsed time', tme)
+    print('Starting optimization')
 
 
-# opt_init, opt_update, get_params = optimizers.sgd(0.0025)
-# opt_state = opt_init(model.weights)
-# 
-# loss = jax.jit(model.loss)
-# for i in range(1000):
-#     value, grads = jax.value_and_grad(loss)(get_params(opt_state))
-#     opt_state = opt_update(i, grads, opt_state)
-#     print(i,value)
-# 
-# weights = get_params(opt_state)
-# weights = model.weights
+    def loss_grad(w):
+        l, gr = lossgrad_compiled(jnp.array(w))
+        return np.array( l.to_py() ), np.array( gr.to_py() ) 
 
-x,y = np.meshgrid(np.linspace(0,1,100),np.linspace(0,1,100))
-ys = np.concatenate((x.flatten()[:,None],y.flatten()[:,None]),1)
-xy1 = geom1(ys)
-xy2 = geom2(ys)
+    tme = datetime.datetime.now()
+    #results = jax.scipy.optimize.minimize(loss_grad, x0 = weights_vector, method = 'bfgs', options = {'maxiter': 10})
+    # result = scipy.optimize.minimize(loss_grad, x0 = w0.to_py(), method = 'BFGS', jac = True, tol = 1e-8, options = {'disp' : True, 'maxiter' : 400}, callback = lambda x: print(loss_compiled(x)))
+    result = scipy.optimize.minimize(loss_grad, x0 = weights.to_py(), method = 'L-BFGS-B', jac = True, tol = 1e-11, options = {'disp' : False, 'maxiter' : 4000, 'iprint': 0})
+    tme = datetime.datetime.now() - tme
 
-u1 = model.solution1(weights, ys).reshape(x.shape)
-u2 = model.solution2(weights, ys).reshape(x.shape)
+    weights = model.weights_unravel(jnp.array(result.x))
 
-fig = plt.figure()
-ax = plt.axes(projection ='3d')
-ax.plot_surface(xy1[:,0].reshape(x.shape), xy1[:,1].reshape(x.shape), u1, cmap ='viridis', vmin = 0, vmax = model.U, edgecolor =None)
-ax.plot_surface(xy2[:,0].reshape(x.shape), xy2[:,1].reshape(x.shape), u2, cmap ='viridis', vmin = 0, vmax = model.U, edgecolor =None)
-plt.show()
-
-
-t = np.linspace(0,1,1000)
-xy1 = geom1(np.concatenate((t[:,None]*0,t[:,None]),1))
-xy2 = geom2(np.concatenate((t[:,None]*0,t[:,None]),1))
-
-uline1 = model.solution1(weights,np.concatenate((t[:,None]*0+0.5,t[:,None]),1)).flatten()
-uline2 = model.solution2(weights,np.concatenate((t[:,None]*0+0.5,t[:,None]),1)).flatten()
-
-a2 = model.U/(np.log(Ro)-np.log(R)+model.eps2/model.eps1*np.log(R/r))
-uref1 = lambda x: a2*model.eps2/model.eps1*np.log(np.sqrt(x[:,0]**2+x[:,1]**2)/r)
-uref2 = lambda x: a2*(np.log(np.sqrt(x[:,0]**2+x[:,1]**2)/R)+model.eps2/model.eps1*np.log(R/r))
     
-plt.figure()
-plt.plot(xy1[:,0],uline1)
-plt.plot(xy2[:,0],uline2)
-plt.plot(xy1[:,0],uref1(xy1))
-plt.plot(xy2[:,0],uref2(xy2))
-# plt.plot(xy[:,0],np.log(xy[:,0]/R)/np.log(r/R))
+    results['parameters'] = result.x.size
+    results['time'] = tme.total_seconds()
+    results['fun_calls'] = result.nfev
+    results['fval'] = result.fun
+    # opt_init, opt_update, get_params = optimizers.sgd(0.0025)
+    # opt_state = opt_init(model.weights)
+    # 
+    # loss = jax.jit(model.loss)
+    # for i in range(1000):
+    #     value, grads = jax.value_and_grad(loss)(get_params(opt_state))
+    #     opt_state = opt_update(i, grads, opt_state)
+    #     print(i,value)
+    # 
+    # weights = get_params(opt_state)
+    # weights = model.weights
 
-plt.figure()
-plt.plot(xy1[:,0],np.log10(np.abs(uline1-uref1(xy1))))
-plt.plot(xy2[:,0],np.log10(np.abs(uline2-uref2(xy2))))
+    a2 = model.U/(np.log(Ro)-np.log(R)+model.eps2/model.eps1*np.log(R/r))
+    uref1 = lambda x: a2*model.eps2/model.eps1*np.log(np.sqrt(x[:,0]**2+x[:,1]**2)/r)
+    uref2 = lambda x: a2*(np.log(np.sqrt(x[:,0]**2+x[:,1]**2)/R)+model.eps2/model.eps1*np.log(R/r))
 
-integral1 = np.sum((model.solution1(weights,model.points['ys']).flatten()-uref1(geom1(model.points['ys'])))**2*model.points['omega1']*model.points['ws']) 
-integral2 = np.sum((model.solution2(weights,model.points['ys']).flatten()-uref2(geom2(model.points['ys'])))**2*model.points['omega2']*model.points['ws']) 
-inttotal = np.sum((model.solution1(weights,model.points['ys']).flatten())**2*model.points['omega1']*model.points['ws']) + np.sum((model.solution2(weights,model.points['ys']).flatten())**2*model.points['omega2']*model.points['ws'])
+    integral1 = np.sum((model.solution1(weights,model.points['ys']).flatten()-uref1(geom1(model.points['ys'])))**2*model.points['omega1']*model.points['ws']) 
+    integral2 = np.sum((model.solution2(weights,model.points['ys']).flatten()-uref2(geom2(model.points['ys'])))**2*model.points['omega2']*model.points['ws']) 
+    inttotal = np.sum((model.solution1(weights,model.points['ys']).flatten())**2*model.points['omega1']*model.points['ws']) + np.sum((model.solution2(weights,model.points['ys']).flatten())**2*model.points['omega2']*model.points['ws'])
 
-error = np.sqrt(integral1+integral2)/np.sqrt(inttotal)
-print('Relative error is %e' % error)
+    error = np.sqrt(integral1+integral2)/np.sqrt(inttotal)
+    results['error_L2'] = error
+    
+    print('Relative error is %e' % error)
+    
+    return results
+
+results = []
+for nl in [3,4,5,6,7,8,9]:
+    res = simmulation(nl = nl)
+    results.append(res)
+    
+import pandas as pd
+df = pd.DataFrame.from_dict(results)
+print(df)
+
+results = []
+for N in [30,40,50,60,70,80,90,100,120]:
+    res = simmulation(N = [N,N])
+    results.append(res)
+    
+import pandas as pd
+df = pd.DataFrame.from_dict(results)
+print(df)
--- quad_2d.py
+++ quad_2d.py
@@ -14,7 +14,7 @@ import scipy.optimize
 
 from jax.config import config
 config.update("jax_enable_x64", True)
-rnd_key = jax.random.PRNGKey(123)
+rnd_key = jax.random.PRNGKey(1234)
 
 def create_geometry(key, scale = 1):
     scale = scale
@@ -67,22 +67,150 @@ def create_geometry(key, scale = 1):
     basis2 = pinns.bspline.BSplineBasis(np.array([0,1]),2)
     
     weights = np.ones(knots.shape[:2])
-    weights[1,1]
+    weights[1,1] = np.sin((np.pi-alpha)/2)
     geom3 = pinns.geometry.PatchNURBS([basis1, basis2], knots, weights, key)
      
-    return  geom1, geom2, geom3 
+    return  geom1, geom3, geom2
 
 geom1, geom2, geom3 = create_geometry(rnd_key)
 
-pts = geom2.sample_inside(1000)
+pts = geom1.sample_inside(10000)
 
 plt.figure()
 plt.scatter(pts[:,0], pts[:,1], s = 1)
 
-pts = geom1.sample_inside(1000)
+pts = geom2.sample_inside(10000)
 plt.scatter(pts[:,0],pts[:,1], s = 1)
 plt.show()
 
-pts = geom3.sample_inside(1000)
+pts = geom3.sample_inside(10000)
 plt.scatter(pts[:,0],pts[:,1], s = 1)
 plt.show()
+
+def interface_function2d(nd, endpositive, endzero, nn):
+
+    faux = lambda x: ((x-endzero)**2/(endpositive-endzero)**2)
+    if nd == 0:
+        fret = lambda ws, x: (nn(ws, x[...,1][...,None]).flatten()*faux(x[...,0]))[...,None]
+    else:
+        fret = lambda ws, x: (nn(ws, x[...,0][...,None]).flatten()*faux(x[...,1]))[...,None]
+    return fret
+
+class Model(pinns.PINN):
+    def __init__(self, rand_key):
+        super().__init__()
+        self.key = rand_key
+
+        N = [64,128]
+        nl = 4
+        
+        block = stax.serial(stax.FanOut(2),stax.parallel(stax.serial(stax.Dense(nl), stax.Tanh, stax.Dense(nl), stax.Tanh),stax.Dense(nl)),stax.FanInSum)
+    
+        self.add_neural_network('u1',stax.serial(block,block,block,block,block,stax.Dense(1)),(-1,2)) # iron
+        self.add_neural_network('u2',stax.serial(block,block,block,block,block,stax.Dense(1)),(-1,2)) # air 
+        self.add_neural_network('u3',stax.serial(block,block,block,block,block,stax.Dense(1)),(-1,2)) # copper
+        self.add_neural_network('u12',stax.serial(stax.Dense(nl),stax.Tanh,stax.Dense(nl),stax.Tanh,stax.Dense(1)),(-1,1))
+        self.add_neural_network('u13',stax.serial(stax.Dense(nl),stax.Tanh,stax.Dense(nl),stax.Tanh,stax.Dense(1)),(-1,1))
+        self.add_neural_network('u23',stax.serial(stax.Dense(nl),stax.Tanh,stax.Dense(nl),stax.Tanh,stax.Dense(1)),(-1,1))
+        
+
+        self.init_points(N)
+        
+        self.interface12 = interface_function2d(0,1.0,0.0,self.neural_networks['u12'])
+        self.interface21 = interface_function2d(1,1.0,0.0,self.neural_networks['u12'])
+        self.interface23 = interface_function2d(0,1.0,0.0,self.neural_networks['u23'])
+        self.interface32 = interface_function2d(1,0.0,1.0,self.neural_networks['u23'])
+        self.eps1 = 1/6
+        self.eps2 = 1/2
+        
+    def init_points(self, N):
+        
+        Knots = np.meshgrid(np.polynomial.legendre.leggauss(N[0])[0]*0.5+0.5, np.polynomial.legendre.leggauss(N[1])[0]*0.5+0.5)
+        ys = np.concatenate(tuple([k.flatten()[:,None] for k in Knots]),-1)
+        Weights = np.kron(np.polynomial.legendre.leggauss(N[0])[1]*0.5, np.polynomial.legendre.leggauss(N[1])[1]*0.5)
+        # ys = np.random.rand(5000,2)*2-1
+        # Weights = np.ones((5000))/5000*4
+        
+        self.points = { 'ys' : ys , 'ws' : Weights}
+        DGys = geom1._eval_omega(ys)
+        Inv = np.linalg.inv(DGys)
+        det = np.abs(np.linalg.det(DGys))
+        self.points['K1'] = np.einsum('mij,mjk,m->mik',Inv,np.transpose(Inv,[0,2,1]),det)
+        self.points['omega1'] = det
+       
+        DGys = geom2._eval_omega(ys)
+        Inv = np.linalg.inv(DGys)
+        det = np.abs(np.linalg.det(DGys))
+        self.points['K2'] = np.einsum('mij,mjk,m->mik',Inv,np.transpose(Inv,[0,2,1]),det)
+        self.points['omega2'] = det
+        
+        
+    def solution1(self, ws, x):
+        # iron
+        u = self.neural_networks['u1'](ws['u1'],x)*0
+        v = ((x[...,0] - 1)*(x[...,0] + 0)*(x[...,1] - 1))[...,None]
+        w =  self.interface12(ws['u12'],x)
+        return u*v+w
+    
+    def solution2(self, ws, x):
+        
+        u = self.neural_networks['u2'](ws['u2'],x)*0
+        v = ((x[...,1] - 1)*(x[...,1] + 0)*(x[...,0] -1))[...,None]
+        w = self.interface21(ws['u12'],x) #+self.interface23(ws['u23'],x)
+        return u*v+w
+    
+    def solution3(self, ws, x):
+        
+        u = self.neural_networks['u1'](ws['u3'],x)*0
+        v = ((x[...,1] - 1)*(x[...,1] + 0)*(x[...,0]-1))[...,None]
+        w =  self.interface32(ws['u23'],x)
+        return u*v + w
+        
+    def loss_pde(self, ws):
+        grad1 = pinns.operators.gradient(lambda x : self.solution1(ws,x))(self.points['ys'])[...,0,:]
+        grad2 = pinns.operators.gradient(lambda x : self.solution2(ws,x))(self.points['ys'])[...,0,:]
+        
+        
+        
+        lpde1 = 0.5*self.eps1*jnp.dot(jnp.einsum('mi,mij,mj->m',grad1,self.points['K1'],grad1), self.points['ws'])  
+        lpde2 = 0.5*self.eps2*jnp.dot(jnp.einsum('mi,mij,mj->m',grad2,self.points['K2'],grad2), self.points['ws'])  
+        return lpde1+lpde2
+
+    def loss(self, ws):
+        lpde = self.loss_pde(ws)
+        return lpde
+    
+
+rnd_key = jax.random.PRNGKey(123)
+model = Model(rnd_key)
+w0 = model.init_unravel()
+weights = model.weights 
+
+x,y = np.meshgrid(np.linspace(0,1,100),np.linspace(0,1,100))
+ys = np.concatenate((x.flatten()[:,None],y.flatten()[:,None]),1)
+xy1 = geom1(ys)
+xy2 = geom2(ys)
+xy3 = geom3(ys)
+
+u1 = model.solution1(weights, ys).reshape(x.shape)
+u2 = model.solution2(weights, ys).reshape(x.shape)
+u3 = model.solution3(weights, ys).reshape(x.shape)
+
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+ax.plot_surface(xy1[:,0].reshape(x.shape), xy1[:,1].reshape(x.shape), u1, cmap ='viridis', vmin = -0.3, vmax = 0.3, edgecolor =None)
+ax.plot_surface(xy2[:,0].reshape(x.shape), xy2[:,1].reshape(x.shape), u2, cmap ='viridis', vmin = -0.3, vmax = 0.3, edgecolor =None)
+#ax.plot_surface(xy3[:,0].reshape(x.shape), xy3[:,1].reshape(x.shape), u3, cmap ='viridis', vmin = -0.2, vmax = 0.2, edgecolor =None)
+plt.show()
+
+x1,_ = geom3[0,:].importance_sampling(1000)
+x2,_ = geom3[:,0].importance_sampling(1000)
+x3,_ = geom3[1,:].importance_sampling(1000)
+x4,_ = geom3[:,1].importance_sampling(1000)
+
+
+plt.figure()
+plt.scatter(x1[:,0],x1[:,1])
+plt.scatter(x2[:,0],x2[:,1])
+plt.scatter(x3[:,0],x3[:,1])
+plt.scatter(x4[:,0],x4[:,1])
